1636611e81e482d1461a920c01dd07a828a880a6
Marian Munoz <mm@MacBook-Pro-de-Marian.local>
2021-01-20 23:11:38 -0600

    packages installed

---
+++ productsdistribution/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+// import Vue from 'vue'
+// import VueRouter from 'vue-router'
+
+// Vue.use(VueRouter)
