733f3b7123c3a8a5485a1ae5c9ce7bf2175c4e6b
Marian Munoz <mm@MacBook-Pro-de-Marian.local>
2021-02-04 17:17:18 -0600

    changes

--- products/deploy.sh
+++ products/deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:MM46/productsDistribution.git master:gh-pages
+git push -f https://github.com/MM46/productsDistribution.git master:gh-pages
 cd -
